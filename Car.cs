d41902762cfa8bfb5cedef80dee73a433d0981ea
NAumenko12 <143539153+NAumenko12@users.noreply.github.com>
2024-03-06 19:21:52 +0700

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carss
+{
+    public abstract class Car
+    {
+        public string Name { get; set; }
+        public int MaxSpeed { get; set; }
+        public int Distance { get; set; }
+        public int CurrentSpeed { get; set; }
+
+        public Car(string name, int maxSpeed, int distance)
+        {
+            Name = name;
+            MaxSpeed = maxSpeed;
+            Distance = distance;
+            CurrentSpeed = 0;
+        }
+
+        public abstract void Move();
+    }
+
+    public class SportsCar : Car
+    {
+        public SportsCar(string name, int maxSpeed, int distance) : base(name, maxSpeed, distance) { }
+
+        public override void Move()
+        {
+            Accelerate();
+            Distance += CurrentSpeed;
+        }
+
+        public void Accelerate()
+        {
+            if (CurrentSpeed < MaxSpeed)
+            {
+                CurrentSpeed += 20;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    CurrentSpeed = MaxSpeed;
+                }
+            }
+        }
+    }
+
+    public class Truck : Car
+    {
+        public Truck(string name, int maxSpeed, int distance) : base(name, maxSpeed, distance) { }
+
+        public override void Move()
+        {
+            Accelerate();
+            Distance += CurrentSpeed;
+        }
+
+        public void Accelerate()
+        {
+            if (CurrentSpeed < MaxSpeed)
+            {
+                CurrentSpeed += 5;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    CurrentSpeed = MaxSpeed;
+                }
+            }
+        }
+    }
+
+    public abstract class CarFactory
+    {
+        public abstract Car CreateCar(string name, int maxSpeed, int distance);
+    }
+
+    public class SportsCarFactory : CarFactory
+    {
+        public override Car CreateCar(string name, int maxSpeed, int distance)
+        {
+            return new SportsCar(name, maxSpeed, distance);
+        }
+    }
+
+    public class TruckFactory : CarFactory
+    {
+        public override Car CreateCar(string name, int maxSpeed, int distance)
+        {
+            return new Truck(name, maxSpeed, distance);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Carss;
+namespace CarRace
+{
+    public class Program
+    {
+        static void Main()
+        {
+            CarFactory sportsCarFactory = new SportsCarFactory();
+            Car car1 = sportsCarFactory.CreateCar("Car1", 150, 0);
+
+            CarFactory truckFactory = new TruckFactory();
+            Car car2 = truckFactory.CreateCar("Truck1", 120, 0);
+
+            List<Car> cars = new List<Car>
+             {
+            car1,
+            car2
+             };
+
+            while (true)
+            {
+                Console.Clear();
+                foreach (var car in cars)
+                {
+                    car.Move();
+                    Console.WriteLine($"Car: {car.Name}, Speed: {car.CurrentSpeed}, Distance: {car.Distance}");
+                }
+
+                if (cars.TrueForAll(c => c.Distance >= 1000))
+                {
+                    Console.WriteLine("\nRace finished!");
+                    cars.Sort((x, y) => y.Distance.CompareTo(x.Distance));
+                    for (int i = 0; i < cars.Count; i++)
+                    {
+                        Console.WriteLine($"Position {i + 1}: {cars[i].Name}");
+                    }
+                    break;
+                }
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
